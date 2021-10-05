5de659580844a46c446f8bb45bac1790949ba227
Ilya Erlingas <54685842+Ilyadevin@users.noreply.github.com>
2021-10-05 21:43:42 +0300

    optimising imports

---
+++ settings/basic_settings.py
@@ -0,0 +1,9 @@
+import csv
+import json
+import logging
+import os
+import re
+from datetime import datetime
+from urllib.parse import quote
+import telebot
+from telebot import types
--- settings/settings.py
+++ settings/settings.py
@@ -1,13 +1,16 @@
-import csv
-import json
-import logging
-import os
-import re
-from datetime import datetime
-from urllib.parse import quote
+from basic_settings import *
 
-import telebot
-from telebot import types
+
+def get_keyboard_fab():
+    buttons = [
+        types.InlineKeyboardButton(text="Математика", callback_data=callback_numbers),
+        types.InlineKeyboardButton(text="Информатика", callback_data=callback_numbers),
+        types.InlineKeyboardButton(text="Инностранный", callback_data=callback_numbers)
+    ]
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    keyboard.add(*buttons)
+    return keyboard
+
 
 # logger settings
 
@@ -67,10 +70,48 @@ def markups(message):
             bot.send_message(message.chat.id, "Отладка, функция пока не работает")
         elif message.text == 'Программирование':
             programming(message)
-        elif message.text == 'Математика':
-            math_(message)
         elif message.text == 'Об авторе':
             about(message)
+
+
+@bot.message_handler(commands=['math_'])
+def keyboard(message):
+    bot.send_message(message.chat.id, 'Кнопка "Математика" - работает')
+
+
+# fabnum - префикс, action - название аргумента, которым будем передавать значение
+callback_numbers = CallbackData("fabnum", "action")
+
+
+async def update_num_text_fab(message: types.Message, new_value: int):
+    with suppress(MessageNotModified):
+        await message.edit_text(f"Укажите число: {new_value}", reply_markup=get_keyboard_fab())
+
+
+@dp.message_handler(commands="numbers_fab")
+async def cmd_numbers(message: types.Message):
+    user_data[message.from_user.id] = 0
+    await message.answer("Укажите число: 0", reply_markup=get_keyboard_fab())
+
+
+@dp.callback_query_handler(callback_numbers.filter(action=["incr", "decr"]))
+async def callbacks_num_change_fab(call: types.CallbackQuery, callback_data: dict):
+    user_value = user_data.get(call.from_user.id, 0)
+    action = callback_data["action"]
+    if action == "incr":
+        user_data[call.from_user.id] = user_value + 1
+        await update_num_text_fab(call.message, user_value + 1)
+    elif action == "decr":
+        user_data[call.from_user.id] = user_value - 1
+        await update_num_text_fab(call.message, user_value - 1)
+    await call.answer()
+
+
+@dp.callback_query_handler(callback_numbers.filter(action=["finish"]))
+async def callbacks_num_finish_fab(call: types.CallbackQuery):
+    user_value = user_data.get(call.from_user.id, 0)
+    await call.message.edit_text(f"Итого: {user_value}")
+    await call.answer()
 
 
 bot.polling(none_stop=True)
