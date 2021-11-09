8356ea811c067c420cdce265d634b8a5e26d0baa
Ilya Erlingas <54685842+Ilyadevin@users.noreply.github.com>
2021-11-09 10:35:49 +0300

    opt

--- settings/basic_settings.py
+++ settings/basic_settings.py
@@ -9,6 +9,24 @@ import telebot
 from telebot import types
 import time
 
+def keyboard_reply():
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    item1 = types.KeyboardButton("🎦 Ищем видео!")
+    item2 = types.KeyboardButton("Об авторе")
+    markup.add(item1, item2)
+    return markup
+
+
+def keyboard_inline():
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    item1 = types.InlineKeyboardButton("Математика базовый уровень", callback_data='mathv_b')
+    item2 = types.InlineKeyboardButton("Математика профильная", callback_data='mathv_p')
+    item3 = types.InlineKeyboardButton("Информатика", callback_data='inf')
+    item4 = types.InlineKeyboardButton("Физика", callback_data='ph')
+    item5 = types.InlineKeyboardButton("В предыдущее меню", callback_data='main_menu')
+    markup.add(item1, item2, item3, item4, item5)
+    return markup
+
 
 def token_get():
     with open("config.json", 'r', encoding='utf-8') as file:
@@ -17,14 +35,12 @@ def token_get():
 
 
 list_of_links = \
-    {"base":
+    {"mbase":
         [
             "https://www.youtube.com/watch?v=st_wW14IqQE&list=PLzJLbwhmP96PjAAG0fSU5Z9Bf2949Frar&index=1",
-            "https://www.youtube.com/watch?v=X4VzjabpqbU&list=PLzJLbwhmP96PjAAG0fSU5Z9Bf2949Frar&index=2",
         ],
-        "profile_1":
+        "mprofile":
             [
                 "https://www.youtube.com/watch?v=NABPt62mcPs&list=PLzJLbwhmP96PtqZR8BunCNLqFQb6T8bQF&index=1",
-                "https://www.youtube.com/watch?v=ePjuT9DKKMo&list=PLzJLbwhmP96PtqZR8BunCNLqFQb6T8bQF&index=2",
             ]
     }
--- settings/settings.py
+++ settings/settings.py
@@ -1,18 +1,11 @@
+import settings.basic_settings
 from settings.basic_settings import *
 
 bot = telebot.TeleBot("2029007087:AAHQVbeZofoMALxBCsa_bmwAS0uU6rEchvk")
 
 
-# Обработка выхода из меню
-
-
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-    item2 = types.KeyboardButton("🎦 Ищем видео!")
-    item3 = types.KeyboardButton("Об авторе")
-
-    markup.add(item2, item3)
     time.sleep(1)
     bot.send_message(message.chat.id,
                      "Добро пожаловать, "
@@ -20,28 +13,24 @@ def welcome(message):
                      "Я - <b>{1.first_name}</b>, \n"
                      "Вместе с сервисом ЕГЭ с ЮКлэва (YouClever) мы поможем вам сдать ЕГЭ на 100!"
                      .format(message.from_user, bot.get_me()),
-                     parse_mode='html', reply_markup=markup)
+                     parse_mode='html', reply_markup=settings.basic_settings.keyboard_reply())
 
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
     if message.chat.type == 'private':
         if message.text == '🎦 Ищем видео!':
-            markup = types.InlineKeyboardMarkup(row_width=2)
-            item1 = types.InlineKeyboardButton("Математика БАЗА", callback_data='mathv_b')
-            item2 = types.InlineKeyboardButton("Математика Профиль", callback_data='mathv_p')
 
-            markup.add(item1, item2)
             time.sleep(1)
             bot.send_message(message.chat.id, "На какую тему нужно видео?\n" "⤵",
-                             parse_mode='html', reply_markup=markup)
+                             parse_mode='html', reply_markup=settings.basic_settings.keyboard_inline())
         elif message.text == 'Об авторе':
             bot.send_message(
                 message.chat.id,
                 'Бот создан на базе библиотеки "telebot"\n'
                 '\n'
                 'При возникновении ошибок логи автоматически отправляются контактному лицу\n'
-                'С предложениями и отзывами можно обратится к автору - @erlihigh'
+                'С предложениями и отзывами можно обратится к автору'
             )
 
 
@@ -49,81 +38,30 @@ def lalala(message):
 def callback_inline(call):
     try:
         if call.message:
-            # Удаление клавиатуры
-            markup = types.InlineKeyboardMarkup(row_width=2)
-            item1 = types.InlineKeyboardButton("Да", callback_data='yes')
-            item2 = types.InlineKeyboardButton("Нет", callback_data='no')
-
-            markup.add(item1, item2)
-            bot.edit_message_text(
-                chat_id=call.message.chat.id, message_id=call.message.message_id,
-                text="Отладка завершена", reply_markup=None
-            )
-
+            bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
             if call.data == 'mathv_b':
                 bot.send_message(call.message.chat.id, 'Далее будут выведены видео с разбором '
                                                        'варианта ЕГЭ по базовой математике')
                 time.sleep(1)
-                for i in range(1, len(list_of_links["base"])):
-                    video = list_of_links["base"][i]
-                    time.sleep(1)
+                for video_base in range(0, len(list_of_links["mbase"])):
+                    video = list_of_links["mbase"][video_base]
+                    time.sleep(1.5)
                     bot.send_message(call.message.chat.id, video)
-                    bot.send_message(
-                        call.message.chat.id, "Вывести следующее видео?\n" "⤵",
-                        reply_markup=markup,
-                        parse_mode='html'
-
-                    )
-                    # a = 0
-                    # if a == 0:
-                    #     @bot.callback_query_handler(func=lambda call_: True)
-                    #     def call_back_end_of_array(call_):
-                    #         if call_.data:
-                    #             if call_.data == 'yes':
-                    #                 a = 1
-                    #                 return True
-                    #             elif call_.data == 'no':
-                    #                 a = 0
-                    #                 return False
-                    # elif a == 1:
-                    #     a = 0
-                    #     continue
-
-                    # if call_back_end_of_array(call) is True:
-                    #   print("дошло сюда")
-                    #  break
-                    # else:
-                    #   markup = types.InlineKeyboardMarkup(row_width=2)
-                    #  item1 = types.InlineKeyboardButton("Математика БАЗА", callback_data='mathv_b')
-                    # item2 = types.InlineKeyboardButton("Математика Профиль", callback_data='mathv_p')
-                    # markup.add(item1, item2)
-                    # bot.send_message(
-                    #    call.message.chat.id, "Выход в предыдущее меню:",
-                    #   parse_mode='html', reply_markup=markup
-                    # )
-                    # break
-                # time.sleep(1)
-                # bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                #                     text="Отладка завершена", reply_markup=None)
-            # elif call.data == 'mathv_p':
-            #     # bot.send_message(call.message.chat.id, 'Нужен разбор первой или второй части?')
-            #     time.sleep(1)
-            #     bot.send_message(call.message.chat.id, 'Разбор 1 части профильного варианта математики')
-            #     for i in range(len(list_of_links["profile_1"])):
-            #         video = list_of_links["profile_1"][i]
-            #         time.sleep(5)
-            #         bot.send_message(call.message.chat.id, video)
-            #     time.sleep(1)
-            #     bot.send_message(call.message.chat.id, 'Разбор 2 части профильного варианта математики')
-            #     for i in range(len(list_of_links["profile_2"])):
-            #         video = list_of_links["profile_2"][i]
-            #         time.sleep(5)
-            #         bot.send_message(call.message.chat.id, video)
-            # bot.edit_message_text(
-            #     chat_id=call.message.chat.id, message_id=call.message.message_id,
-            #     text="Отладка завершена", reply_markup=None
-            # )
-
+            elif call.data == 'mathv_p':
+                for video_profil in range(len(list_of_links["mprofile"])):
+                    video = list_of_links["profile_1"][video_profil]
+                    time.sleep(1.5)
+                    bot.send_message(call.message.chat.id, video)
+            elif call.data == 'inf':
+                for video_inf in range(len(list_of_links["inf"])):
+                    video = list_of_links["profile_1"][video_inf]
+                    time.sleep(1.5)
+                    bot.send_message(call.message.chat.id, video)
+            elif call.data == 'ph':
+                for video_ph in range(len(list_of_links["ph"])):
+                    video = list_of_links["profile_1"][video_ph]
+                    time.sleep(1.5)
+                    bot.send_message(call.message.chat.id, video)
     except Exception as e:
         bot.send_message(445431715, f"Возникла ошибка у пользователя - {call.message.message_id} \n"
                                     f"код ошибки {str(e)}")
